ba42ec21f2547de3417347357bdb9fdef5baff85
Kevin Lundberg <kevinrlundberg@gmail.com>
2015-05-08 13:27:43 -0400

    project fixes

--- Autolayout-CollectionViewExample/ViewController.swift
+++ Autolayout-CollectionViewExample/ViewController.swift
@@ -16,19 +16,25 @@ class ViewController: UICollectionViewController, UICollectionViewDelegateFlowLa
         super.viewDidLoad()
 
         sizingCell = collectionView?.dequeueReusableCellWithReuseIdentifier("Cell", forIndexPath: NSIndexPath(forItem: 0, inSection: 0)) as? UICollectionViewCell
+
+        collectionView?.reloadData()
     }
 
     func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
 
         if let sizingCell = sizingCell { // if the sizing cell is nil, we're in viewDidLoad and we're dequeueing the initial cell used for sizing.
+
+            // set the width we want the cell to be in hopes that the cell will size vertically to match the aspect ratio in the cell.
             sizingCell.frame.size.width = collectionView.bounds.size.width
-            sizingCell.setNeedsLayout()
+            sizingCell.contentView.frame = sizingCell.bounds
+            sizingCell.setNeedsLayout() // this is where the problem happens, the cell's height constraints conflict with the aspect ratio and the width, and the aspect ratio constraint is usually broken first.
             sizingCell.layoutIfNeeded()
 
-            let size = sizingCell.systemLayoutSizeFittingSize(UILayoutFittingCompressedSize)
+            let size = sizingCell.contentView.systemLayoutSizeFittingSize(UILayoutFittingCompressedSize)
+            // this size is the original size of the cell due to the constraint problems, not the desired full-width with respected aspect ratio.
             return size
         } else {
-            return CGSize(width: 150, height: 100) // dummy size for the original sizing cell
+            return CGSize(width: 150, height: 100) // dummy size for the original sizing cell to be sized at temporarily
         }
     }
 
